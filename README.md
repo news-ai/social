d6e026fed18403d6acfb122a1340495c3ec4bcef
Abhi Agarwal <me@abhiagarwal.com>
2016-09-16 17:45:46 -0400

    Adding some code to get data from http trigger

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # social
-Getting data from rss, and twitter through Google Cloud Functions
+
+Getting data from RSS, and twitter through Google Cloud Functions.
+
+Feed loops through all the feeds in the API that haven't been processed. It calls the RSS function for each feed. The RSS function gets the RSS headlines and writes them to ES.
--- rss/rss.js
+++ rss/rss.js
@@ -1,5 +1,6 @@
 var feedparser = require('feedparser-promised');
 var Q = require('q');
+var mocks = require('node-mocks-http');
 
 function getFeedFromUrl(url) {
     var deferred = Q.defer();
@@ -22,9 +23,31 @@ function getFeedFromUrl(url) {
     return deferred.promise;
 }
 
-getFeedFromUrl('http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml')
-    .then(function(data) {
-        console.log(data);
-    }, function(error) {
-        console.log(error);
-    });
+function getContents(req, res) {
+    var deferred = Q.defer();
+
+    getFeedFromUrl(req.body.url)
+        .then(function(data) {
+            console.log(data);
+            res.status(200).end();
+        }, function(error) {
+            console.error(error);
+            deferred.reject(new Error(error));
+            res.status(500).send(error);
+        });
+
+    return deferred.promise;
+}
+
+exports.processFeed = function processFeed(req, res) {
+    return getContents();
+};
+
+function testProcess() {
+    req = mocks.createRequest();
+    res = mocks.createResponse();
+    req.body.url = 'http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml';
+    return getContents(req, res);
+};
+
+testProcess();
