1079a346b55239d6088e95e9466b0bff564d88ea
Abhi Agarwal <me@abhiagarwal.com>
2016-09-16 18:28:27 -0400

    Writing to ES works

--- rss/rss.js
+++ rss/rss.js
@@ -1,6 +1,14 @@
 var feedparser = require('feedparser-promised');
+var elasticsearch = require('elasticsearch');
 var Q = require('q');
 var mocks = require('node-mocks-http');
+
+// Instantiate a elasticsearch client
+var client = new elasticsearch.Client({
+    host: 'https://newsai:XkJRNRx2EGCd6@search.newsai.org',
+    // log: 'trace',
+    rejectUnauthorized: false
+});
 
 function getFeedFromUrl(url) {
     var deferred = Q.defer();
@@ -30,7 +38,31 @@ function getFeedFromUrl(url) {
 function addToElastic(contactId, content) {
     var deferred = Q.defer();
 
-    console.log(contactId);
+    var esActions = [];
+    for (var i = content.length - 1; i >= 0; i--) {
+        var indexRecord = {
+            index: {
+                _index: 'headlines',
+                _type: 'headline'
+            }
+        };
+        var dataRecord = content[i];
+        dataRecord.ContactId = contactId;
+        esActions.push(indexRecord);
+        esActions.push({
+            data: dataRecord
+        });
+    }
+
+    client.bulk({
+        body: esActions
+    }, function(error, response) {
+        if (error) {
+            console.error(error);
+            deferred.resolve(false);
+        }
+        deferred.resolve(true);
+    });
 
     return deferred.promise;
 }
@@ -73,7 +105,7 @@ function testProcess() {
     req = mocks.createRequest();
     res = mocks.createResponse();
     req.body.contactId = 5702224873259008;
-    req.body.url = 'http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml';
+    req.body.url = 'http://nypost.com/author/michael-goodwin/feed/';
     return getContent(req, res);
 };
 
