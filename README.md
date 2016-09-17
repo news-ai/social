08bbd8599cb75fe9451b33f9b5cd807d01e443c8
Abhi Agarwal <me@abhiagarwal.com>
2016-09-16 20:06:10 -0400

    switching back to p/s from http

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Feed loops through all the feeds in the API that haven't been processed. It call
 
 ### RSS
 
-Deploy RSS: `cd rss` then `gcloud alpha functions deploy processRSS --bucket datastore_elastic_api_sync --trigger-http --region us-central1`
+Deploy RSS: `cd rss` then `gcloud alpha functions deploy processRSS --stage-bucket datastore_elastic_api_sync --trigger-topic datastore-process-rss-functions --region us-central1`
 
 Get logs for RSS: `gcloud alpha functions get-logs processRSS`
--- rss/rss.js
+++ rss/rss.js
@@ -1,7 +1,6 @@
 var feedparser = require('feedparser-promised');
 var elasticsearch = require('elasticsearch');
 var Q = require('q');
-var mocks = require('node-mocks-http');
 
 // Instantiate a elasticsearch client
 var client = new elasticsearch.Client({
@@ -68,46 +67,44 @@ function addToElastic(contactId, content) {
     return deferred.promise;
 }
 
-function getContent(req, res) {
+function getContent(data) {
     var deferred = Q.defer();
 
-    getFeedFromUrl(req.body.url)
+    getFeedFromUrl(data.url)
         .then(function(content) {
-            addToElastic(req.body.contactId, content)
+            addToElastic(data.contactId, content)
                 .then(function(status) {
                     if (status) {
                         deferred.resolve(true);
-                        res.status(200).end();
                     } else {
                         var error = 'Elasticsearch add failed';
                         console.error(error);
                         deferred.resolve(false);
-                        res.status(500).send(error);
+                        throw new Error(error);
                     }
                 }, function (error) {
                     console.error(error);
                     deferred.resolve(false);
-                    res.status(500).send();
+                    throw new Error(error);
                 });
         }, function(error) {
             console.error(error);
             deferred.reject(new Error(error));
-            res.status(500).send(error);
+            throw new Error(error);
         });
 
     return deferred.promise;
 }
 
-exports.processRSS = function processRSS(req, res) {
-    return getContent(req, res);
+exports.processRSS = function processRSS(data) {
+    return getContent(data);
 };
 
 function testProcess() {
-    req = mocks.createRequest();
-    res = mocks.createResponse();
-    req.body.contactId = 5702224873259008;
-    req.body.url = 'http://nypost.com/author/michael-goodwin/feed/';
-    return getContent(req, res);
+    var data = {};
+    data.contactId = 4934182044172288;
+    data.url = 'http://pagesix.com/author/cindy-adams/feed/';
+    return getContent(data);
 };
 
 testProcess();
