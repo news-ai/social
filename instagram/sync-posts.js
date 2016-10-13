4ab202b2043c1f8337cd6e9377dc6b5cffff66aa
Abhi Agarwal <me@abhiagarwal.com>
2016-10-12 22:34:02 -0400

    reaching EO ES

--- instagram/sync-posts.js
+++ instagram/sync-posts.js
@@ -23,8 +23,13 @@ var elasticSearchClient = new elasticsearch.Client({
 var sentryClient = new raven.Client('https://666f957c7dd64957996c1b05675a960a:b942eb7df51d4f8780f55b7d4592a39f@sentry.io/105661');
 sentryClient.patchGlobal();
 
-function getInstagramPostsFromLastWeek() {
+function getInstagramPageFromLastWeek(offset) {
     var deferred = Q.defer();
+
+    var dateTo = moment().format('YYYY-MM-DD');
+    var dateFrom = moment().subtract(7, 'd')
+    var lastWeek = dateFrom.format('YYYY-MM-DDTHH:mm:ss');
+    console.log(lastWeek);
 
     elasticSearchClient.search({
         index: 'instagrams',
@@ -38,7 +43,7 @@ function getInstagramPostsFromLastWeek() {
                     "filter": {
                         "range": {
                             "data.CreatedAt": {
-                                "gte": "2016-05-02T14:14:27"
+                                "gte": lastWeek
                             }
                         }
                     }
@@ -50,11 +55,12 @@ function getInstagramPostsFromLastWeek() {
                     "mode": "avg"
                 }
             }],
-            "size": 100
+            "size": 100,
+            "from": 100 * offset,
         }
     }).then(function(resp) {
         var hits = resp.hits.hits;
-        console.log(hits);
+        deferred.resolve(hits);
     }, function(err) {
         console.trace(err.message);
     });
@@ -62,6 +68,21 @@ function getInstagramPostsFromLastWeek() {
     return deferred.promise;
 }
 
-getInstagramPostsFromLastWeek().then(function (data) {
-    console.log(data);
+function getInstagramPostsFromLastWeek(offset, allData) {
+    var deferred = Q.defer();
+
+    getInstagramPageFromLastWeek(offset).then(function(data) {
+        if (data.length === 0) {
+            deferred.resolve(allData);
+        } else {
+            var newData = allData.concat(data);
+            deferred.resolve(getInstagramPostsFromLastWeek(offset + 1, newData));
+        }
+    });
+
+    return deferred.promise;
+}
+
+getInstagramPostsFromLastWeek(0, []).then(function(data) {
+    console.log(data.length);
 });
