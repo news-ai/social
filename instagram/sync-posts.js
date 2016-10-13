06bcf56d9c80bd8fe0544d71cff7a744c5cb0265
Abhi Agarwal <me@abhiagarwal.com>
2016-10-12 23:45:45 -0400

    Finished ES <-> Instagram sync

--- instagram/sync-posts.js
+++ instagram/sync-posts.js
@@ -23,6 +23,102 @@ var elasticSearchClient = new elasticsearch.Client({
 var sentryClient = new raven.Client('https://666f957c7dd64957996c1b05675a960a:b942eb7df51d4f8780f55b7d4592a39f@sentry.io/105661');
 sentryClient.patchGlobal();
 
+function formatToFeed(post, username) {
+    return {
+        'CreatedAt': post.CreatedAt,
+        'Type': 'Instagram',
+
+        // Headlines
+        'Title': '',
+        'Url': '',
+        'Summary': '',
+        'FeedURL': '',
+        'PublicationId': 0,
+
+        // Tweet
+        'TweetId': 0,
+        'Username': '',
+
+        // Tweet + Instagram
+        'Text': post.Caption,
+
+        // Instagram
+        'InstagramUsername': username,
+        'InstagramId': post.InstagramId,
+        'InstagramImage': post.Image,
+        'InstagramVideo': post.Video,
+        'InstagramLink': post.Link,
+        'InstagramLikes': post.Likes,
+        'InstagramComments': post.Comments
+    };
+}
+
+// Add these instagram posts to ElasticSearch
+// username here is the base parent username.
+// Not just a username of any user.
+function addToElastic(posts) {
+    var deferred = Q.defer();
+
+    var esActions = [];
+    // Look through all the instagram data
+    for (var i = posts.data.length - 1; i >= 0; i--) {
+        var newInstagramPost = posts.data[i];
+
+        var newInstagramPostId = newInstagramPost.id;
+        var username = newInstagramPost.username;
+        
+        delete newInstagramPost.id;
+        delete newInstagramPost.username;
+
+        // Add to instagram endpoint
+        var indexRecord = {
+            index: {
+                _index: 'instagrams',
+                _type: 'instagram',
+                _id: newInstagramPostId
+            }
+        };
+        var dataRecord = newInstagramPost;
+        dataRecord.Username = username;
+        esActions.push(indexRecord);
+        esActions.push({
+            data: dataRecord
+        });
+
+        // Add to feeds endpoint
+        indexRecord = {
+            index: {
+                _index: 'feeds',
+                _type: 'feed',
+                _id: newInstagramPostId
+            }
+        };
+        dataRecord = formatToFeed(newInstagramPost, username);
+        esActions.push(indexRecord);
+        esActions.push({
+            data: dataRecord
+        });
+    }
+
+    if (esActions.length > 0) {
+        elasticSearchClient.bulk({
+            body: esActions
+        }, function(error, response) {
+            if (error) {
+                sentryClient.captureMessage(error);
+                deferred.reject(error);
+            }
+            deferred.resolve(true);
+        });
+    } else {
+        var error = 'Nothing was added to ES for username ' + username;
+        sentryClient.captureMessage(error);
+        deferred.reject(error);
+    }
+
+    return deferred.promise;
+}
+
 function getInstagramPageFromEsLastWeek(offset) {
     var deferred = Q.defer();
 
@@ -108,14 +204,59 @@ function getInstagramPostsFromAPI(data) {
     return Q.allSettled(allPromises);
 }
 
-getInstagramPostsFromEsLastWeek(0, []).then(function(data) {
-    getInstagramPostsFromAPI(data).then(function (instagramPosts) {
-        for (var i = instagramPosts.length - 1; i >= 0; i--) {
-            if (instagramPosts[i].state === 'fulfilled') {
-                console.log(instagramPosts[i]);
-            } else {
-                console.log('rejected');
+function syncIGAndES() {
+    var deferred = Q.defer();
+
+    getInstagramPostsFromEsLastWeek(0, []).then(function(data) {
+        getInstagramPostsFromAPI(data).then(function (instagramPosts) {
+            var posts = [];
+
+            for (var i = instagramPosts.length - 1; i >= 0; i--) {
+                if (instagramPosts[i].state === 'fulfilled') {
+                    instagramPosts[i] = instagramPosts[i].value;
+                    var instagramId = [instagramPosts[i].id, instagramPosts[i].owner.id].join('_');
+                    var tags = instagramPosts[i].caption && instagramPosts[i].caption.match(/#[a-z]+/gi) || [];
+
+                    var post = {
+                        'CreatedAt': moment.unix(parseInt(instagramPosts[i].date, 10)).format('YYYY-MM-DDTHH:mm:ss'),
+                        'Video': instagramPosts[i].video_url || '',
+                        'Image': instagramPosts[i].display_src || '',
+                        'Location': instagramPosts[i].location && instagramPosts[i].location.name || '',
+                        'Coordinates': '',
+                        'InstagramId': instagramId || '',
+                        'Caption': instagramPosts[i].caption || '',
+                        'Likes': instagramPosts[i].likes && instagramPosts[i].likes.count || 0,
+                        'Comments': instagramPosts[i].comments && instagramPosts[i].comments.count || 0,
+                        'Link': 'https://www.instagram.com/p/' + instagramPosts[i].code + '/' || '',
+                        'Tags': tags || [],
+                        'id': instagramId || '',
+                        'username': instagramPosts[i].owner && instagramPosts[i].owner.username || '',
+                    };
+
+                    posts.push(post);
+                } else {
+                    console.log('deleted posts');
+                }
             }
-        }
+
+            posts = {
+                data: posts
+            };
+
+            addToElastic(posts).then(function (status) {
+                deferred.resolve(status);
+            }, function (error) {
+                sentryClient.captureMessage(error);
+                deferred.reject(error);
+            })
+        });
     });
-});
+
+    return deferred.promise;
+}
+
+syncIGAndES().then(function (status) {
+    console.log(status);
+}, function (error) {
+    console.error(error);
+})
