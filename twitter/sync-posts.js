066e2d8065a739b4be9f4afb943f8cde8e141e61
Abhi Agarwal <me@abhiagarwal.com>
2016-10-17 21:08:48 -0400

    Small addition to test on prod

--- twitter/sync-posts.js
+++ twitter/sync-posts.js
@@ -6,6 +6,7 @@
 var Q = require('q');
 var elasticsearch = require('elasticsearch');
 var moment = require('moment');
+var Twitter = require('twitter');
 var raven = require('raven');
 
 var twitter = require('./twitter');
@@ -17,9 +18,36 @@ var elasticSearchClient = new elasticsearch.Client({
     rejectUnauthorized: false
 });
 
+// Instantiate a twitter client
+var twitterClient = new Twitter({
+    consumer_key: 'nu83S4GaW4vrsN6gPoTbSvuMy',
+    consumer_secret: 't86zlLxN7mjwHu9OMflX806StaqSFWfLMTOiiFLmOuwI5kUFFE',
+    access_token_key: '758002735547609088-bPZJ1mO8nPfHq52FquOh0tsaWa6Fc28',
+    access_token_secret: 'NIYOhbJZSFzKNRJGVdtPlzMnzKet9bHdwH08ghw9TmzWr'
+});
+
 // Instantiate a sentry client
 var sentryClient = new raven.Client('https://666f957c7dd64957996c1b05675a960a:b942eb7df51d4f8780f55b7d4592a39f@sentry.io/105661');
 sentryClient.patchGlobal();
+
+// Get last 20 tweets for a particular user
+function getTweetsFromId(tweetId) {
+    var deferred = Q.defer();
+
+    twitterClient.get('statuses/show', {
+        id: tweetId
+    }, function(error, tweet, response) {
+        if (!error) {
+            deferred.resolve(tweet);
+        } else {
+            console.error(error);
+            sentryClient.captureMessage(error);
+            deferred.reject(new Error(error));
+        }
+    });
+
+    return deferred.promise;
+}
 
 function syncTwitterAndES() {
     var deferred = Q.defer();
@@ -28,6 +56,17 @@ function syncTwitterAndES() {
     twitter.getTweetsFromEsLastWeek(0, []).then(function(data) {
         // Find the tweets from Twitter API
         console.log(data.length);
+        var tweetIds = []
+        for (var i = 0; i < data.length; i++) {
+            var tweet = {
+                'ESId': data[i]._id,
+                'TwitterId': data[i]._source.data.TweetIdStr
+            };
+            tweetIds.push(tweet);
+        }
+
+        // Go through each tweet and process it again
+        console.log(tweetIds.length);
     }, function (error) {
         sentryClient.captureMessage(error);
         console.error(error);
