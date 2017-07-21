edeec1c4cb9870ce0bd9bd3ca2d86e0cb6ddfd21
Abhi Agarwal <me@abhiagarwal.com>
2017-07-20 22:31:33 -0400

    test deploy

--- md/rss/article.py
+++ md/rss/article.py
@@ -4,8 +4,24 @@ from newspaper import Article
 
 
 def process_article(single_article):
-    link = single_article['link']
-    article = Article(link)
+    # Process article using Newspaper
+    article = Article(single_article['link'])
     article.download()
     article.parse()
-    print article.authors
+    article.nlp()
+
+    # Create profile of article that will
+    # be added to elasticsearch
+    article_details = {
+        '_id': single_article['link'],
+        'url': single_article['link'],
+
+        'title': article.title,
+
+        'top_image': article.top_image,
+
+        'keywords': article.keywords,
+        'summary': article.summary,
+    }
+
+    print article_details
